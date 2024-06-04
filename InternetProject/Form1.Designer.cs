a72018b02a21c7884b1b6400c3ae950bef4d803a
HamSamMike <846957664@qq.com>
2024-06-04 15:24:15 +0800

    独立DownloadData函数以及button1的部分实现

--- InternetProject/Form1.Designer.cs
+++ InternetProject/Form1.Designer.cs
@@ -28,20 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(161, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(492, 25);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
@@ -83,30 +75,37 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(158, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(589, 378);
+            this.richTextBox2.TabIndex = 6;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RichTextBox richTextBox2;
     }
 }
 
--- InternetProject/Form1.cs
+++ InternetProject/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -18,16 +19,41 @@ namespace InternetProject
             InitializeComponent();
         }
 
+        private string DownloadData(string url)
+        {
+            try
+            {
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                request.Credentials = CredentialCache.DefaultCredentials;
+
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+
+                Stream res = response.GetResponseStream();
+                Encoding encoding = Encoding.UTF8;
+
+                StreamReader sr = new StreamReader(res,encoding);
+
+                string str=sr.ReadToEnd();
+                sr.Close();
+
+                res.Close();
+                return str;
+            }
+            finally
+            {
+
+            }
+            return "";
+        }
+
+
         private void button1_Click(object sender, EventArgs e)
         {
             string url = "https://www.hubu.edu.cn/";
             WebClient client = new WebClient();
-            byte[] pageData = client.down
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
+            byte[] pageData = client.DownloadData(url);
+            string pagehtml=Encoding.UTF8.GetString(pageData);
+            richTextBox2.Text = pagehtml;
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -41,6 +67,11 @@ namespace InternetProject
         }
 
         private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
         {
 
         }
